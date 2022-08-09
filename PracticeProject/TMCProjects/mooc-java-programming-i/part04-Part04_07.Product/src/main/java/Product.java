035ea15f9c816761e693f3ff32124dbd45556829
Daniel <crockdc@gmail.com>
2022-08-08 22:06:30 -0400

    Part 4_07 - My solution

--- PracticeProject/TMCProjects/mooc-java-programming-i/part04-Part04_07.Product/src/main/java/Product.java
+++ PracticeProject/TMCProjects/mooc-java-programming-i/part04-Part04_07.Product/src/main/java/Product.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Product {
+public class Product {
+    private double price;
+    private int quantity;
+    private String name;
+
+    public Product(String initialName, double initialPrice, int initialQuantity) {
+        this.price = initialPrice;
+        this.quantity = initialQuantity;
+        this.name = initialName;
+
+    }
+    public void printProduct() {
+        System.out.println(this.name + ", price " + this.price + ", "
+        + this.quantity + " pcs");
+    }
 }
